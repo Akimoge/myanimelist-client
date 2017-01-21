b689959baf499e6ed3515716616f4128d2183f5a
Akimoge <akisenpai@cock.li>
2017-01-21 15:55:33 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+gem install myanimelist_client
+gem install nokogiri
+gem install json
+gem install open-uri
+
+mkdir /etc/mal
+curl -L "https://raw.githubusercontent.com/Akimoge/myanimelist-client/master/anime" > /usr/bin/anime
+curl -L "https://raw.githubusercontent.com/Akimoge/myanimelist-client/master/config.json" > /etc/mal/config.json
