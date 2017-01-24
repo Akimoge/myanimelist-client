ad03bdd4424edfb05fe02c6c1b89d7d77075e96d
Akimoge <admin@orosu.eu>
2017-01-24 07:58:01 +0000

    Updated permissions

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ gem install json
 gem install open-uri
 
 mkdir /etc/mal
+curl -L "https://raw.githubusercontent.com/Akimoge/myanimelist-client/master/manga" > /usr/bin/anime
 curl -L "https://raw.githubusercontent.com/Akimoge/myanimelist-client/master/anime" > /usr/bin/anime
 curl -L "https://raw.githubusercontent.com/Akimoge/myanimelist-client/master/config.json" > /etc/mal/config.json
